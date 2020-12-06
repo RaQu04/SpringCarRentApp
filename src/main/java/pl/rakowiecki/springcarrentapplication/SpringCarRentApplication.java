961498661a99ad1cf7ee2eb89ce88182fb456364
Lukasz Rakowiecki <lukasz.er17@gmail.com>
2020-12-06 20:38:04 +0100

    Git init

---
+++ src/main/java/pl/rakowiecki/springcarrentapplication/SpringCarRentApplication.java
@@ -0,0 +1,13 @@
+package pl.rakowiecki.springcarrentapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCarRentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCarRentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/rakowiecki/springcarrentapplication/SpringCarRentApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.rakowiecki.springcarrentapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCarRentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
